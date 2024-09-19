a17dc0f8443bf82d87bbe7c1444e7295d2eb52ca
eyupcaki <33785497+eyupcaki@users.noreply.github.com>
2024-09-19 16:49:10 +0300

    ....

---
+++ Project1Portfolio/Controllers/MessageController.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using Project1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyPortfolioDbEntities context=new MyPortfolioDbEntities();
+
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var values = context.Message.ToList();
+            return View(values);
+        }
+
+        public ActionResult MessageDetails(int id) 
+        {
+            var value=context.Message.Where(x=>x.MessageId==id).FirstOrDefault();
+            return View(value);
+        }
+
+        public ActionResult MessageStatusChangeToTrue(int id)
+        {
+            var value=context.Message.Where(x=>x.MessageId == id).FirstOrDefault();
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+
+        } 
+        
+        public ActionResult MessageStatusChangeToFalse(int id)
+        {
+            var value=context.Message.Where(x=>x.MessageId == id).FirstOrDefault();
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+
+        }
+    }
+}
